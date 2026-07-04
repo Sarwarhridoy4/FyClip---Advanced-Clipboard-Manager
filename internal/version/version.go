dcf5fe14c86424180b3e1873f66cb7ce0e57572c
Sarwar Hossain <sarwarhridoy4@gmail.com>
2026-07-04 19:52:13 +0600

    chore: bump version to 2.2.3

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@
 package version
 
 // Version is the application version, set during build
-var Version = "2.2.2"
+var Version = "2.2.3"
 
 // BuildTime is the build timestamp, set during build
 var BuildTime = "2026-04-30 00:00:00 UTC"
